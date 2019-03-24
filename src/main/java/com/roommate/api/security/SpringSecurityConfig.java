b8e113d523a3199679bdb5ed55040bf6280392d0
Stefanija <stefanija.mihajlovska@it-labs.com>
2019-03-24 18:14:39 +0100

    Design Home Page

--- src/main/java/com/roommate/api/security/SpringSecurityConfig.java
+++ src/main/java/com/roommate/api/security/SpringSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -36,6 +37,12 @@ public class SpringSecurityConfig  extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(
+               "/resources/**","/images/**");
     }
 
     @Bean
--- src/main/java/com/roommate/api/web/controller/UserController.java
+++ src/main/java/com/roommate/api/web/controller/UserController.java
@@ -13,8 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.jws.WebParam.Mode;
-
 @RestController
 public class UserController {
     @Autowired
